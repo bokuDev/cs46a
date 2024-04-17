c55f2079609310da8fd3add0ee2c0f883c8bc2b6
Canhui <huangcanhui121@gmail.com>
2024-04-16 18:30:50 -0700

    finished participation 12_1

---
+++ lec18/src/PrintArray.java
@@ -0,0 +1,10 @@
+public class Problem1 {
+    // Create an array of integers with the values 10,20,30,40, and 50. Print each value using a for-loop.
+    public static void main(String[] args) {
+        int[] numbers = {10,20,30,40,50};
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
